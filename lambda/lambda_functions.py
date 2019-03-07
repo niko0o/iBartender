5d389b8def21a0aa4e4cda242460a6b9d01b2a0f
niko0o <niko.koivu@gmail.com>
2019-03-07 11:14:48 +0200

    fix lambda function, add error response

--- lambda/lambda_functions.py
+++ lambda/lambda_functions.py
@@ -16,17 +16,25 @@ def build_PlainSpeech(body):
     speech['type'] = 'PlainText'
     speech['text'] = body
     return speech
+
 def lambda_handler(event, context):
     if event['request']['type'] == "LaunchRequest":
-            message = build_PlainSpeech("Welcome to iBartender! P.S. Olli sux cox and dix")
+            message = build_PlainSpeech("Welcome to iBartender! Please order drink, for example, \"Alexa, ask bartender to make me a rum and coke\"")
             return build_response(message)
     elif event['request']['intent']['name'] == "DrinkIntent":
-
-            #drink = ['request']['intent']['slots']['drink']['name']['value']
-            message = build_PlainSpeech("One drink coming!")
-            response = client.publish(
-                topic='ibartender/order',
-                qos=1,
-                payload=json.dumps({"foo":"bar"})
-            )
-            return build_response(message)
+            if event['request']['intent']['slots']['drink']['resolutions']['resolutionsPerAuthority'][0]['status']['code'] == "ER_SUCCESS_NO_MATCH":
+                message = build_PlainSpeech("I can't make that drink yet. Please try another drink")
+                return build_response(message)
+            else:
+                drink = event['request']['intent']['slots']['drink']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['name']
+                drinkId = event['request']['intent']['slots']['drink']['resolutions']['resolutionsPerAuthority'][0]['values'][0]['value']['id']
+                message = build_PlainSpeech("One " + drink + " coming!")
+                response = client.publish(
+                    topic='ibartender/order',
+                    qos=1,
+                    payload=json.dumps({"drink":drinkId})
+                )
+                return build_response(message)
+    else:
+        message = build_PlainSpeech("Sorry, I didn't get that. Please order drink, for example, \"Alexa, ask bartender to make me a rum and coke\"")
+        return build_response(message)
